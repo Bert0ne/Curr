3bb36933732e07d5ad0ea99df009096a957a9ef1
Robert <wybawiciel@gmail.com>
2022-06-25 14:34:49 +0200

    first input done

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-import currCodeAndCountry from './currenciesCodeCountry.js'
+// import currCodeAndCountry from './currenciesCodeCountry.js'
 
-    let currenciesApiData = []; 
-    let currFullData;
+//     let currenciesApiData = []; 
+//     let currFullData;
     
-    fetch(
-        `http://api.nbp.pl/api/exchangerates/tables/a/`
-        )
-    .then(resp => resp.json())
-    .then(data => {
-        currenciesApiData = data[0].rates
-            connectData()
-            console.log(currFullData);
-    });
+//     fetch(
+//         `http://api.nbp.pl/api/exchangerates/tables/a/`
+//         )
+//     .then(resp => resp.json())
+//     .then(data => {
+//         currenciesApiData = data[0].rates
+//             connectData()
+//             console.log(currFullData);
+//     });
 
 
-function connectData() {
-    currenciesApiData.map( el =>{
-        currCodeAndCountry.forEach((elSM, index) => {
-            if(el.code == elSM.code) {
-                currFullData = currenciesApiData
-                currFullData[index].country = currCodeAndCountry[index].country.toLowerCase()
-                currFullData[index].src = `https://countryflagsapi.com/png/${currFullData[index].country}`
-            }
-        })
-    })
-}
+// function connectData() {
+//     currenciesApiData.map( el =>{
+//         currCodeAndCountry.forEach((elSM, index) => {
+//             if(el.code == elSM.code) {
+//                 currFullData = currenciesApiData
+//                 currFullData[index].country = currCodeAndCountry[index].country.toLowerCase()
+//                 currFullData[index].src = `https://countryflagsapi.com/png/${currFullData[index].country}`
+//             }
+//         })
+//     })
+// }
+
