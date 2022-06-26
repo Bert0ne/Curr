f3a0d5771b9f83c33b362dddf6a6985cad614d8b
Robert <wybawiciel@gmail.com>
2022-06-26 22:23:44 +0200

    get data done

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// import currCodeAndCountry from './currenciesCodeCountry.js'
+import currCodeAndCountry from './currenciesCodeCountry.js'
 
-//     let currenciesApiData = []; 
-//     let currFullData;
     
 //     fetch(
 //         `http://api.nbp.pl/api/exchangerates/tables/a/`
@@ -26,9 +24,63 @@
 //     })
 // }
 
+let currenciesApiData = []; 
+let currFullData;
+const LOCAL_STORAGE_DATA = 'countryData'
+const LOCAL_STORAGE_HOUR = 'hourData'
+const d = new Date();
+let hour = d.getHours();
     
-window.addEventListener('DOMContentLoaded', isLocalStorage);
+window.addEventListener('DOMContentLoaded', init());
+
+function init() {
+    isLocalStorage();
+    timerGetRates()
+}
+
+function timerGetRates() {
+    setInterval(() => {
+        getData()
+    }, 3600000);
+}
 
 function isLocalStorage() {
-    
+    const storage = localStorage.getItem(LOCAL_STORAGE_DATA);
+    const timeStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_HOUR))
+
+    if(storage && timeStorage == hour) {
+        currenciesApiData = JSON.parse(storage)
+        
+        console.log(currenciesApiData);
+    } else {
+        getData()
+    }
 }
+
+function getData() {
+    fetch(
+        `http://api.nbp.pl/api/exchangerates/tables/a/`
+        )
+    .then(resp => resp.json())
+    .then(data => {
+        currenciesApiData = data[0].rates
+            connectData()
+            console.log(currFullData);
+            localStorage.setItem(LOCAL_STORAGE_DATA, JSON.stringify(currenciesApiData))
+            localStorage.setItem(LOCAL_STORAGE_HOUR, JSON.stringify(hour))
+    });
+}
+
+function connectData() {
+    currenciesApiData.map( el =>{
+        currCodeAndCountry.forEach((elSM, index) => {
+            if(el.code == elSM.code) {
+                currFullData = currenciesApiData
+                currFullData[index].country = currCodeAndCountry[index].country.toLowerCase()
+                currFullData[index].src = `https://countryflagsapi.com/png/${currFullData[index].country}`
+            }
+        })
+    })
+}
+
+
