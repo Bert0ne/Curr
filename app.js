bdba84f445060428a1373ba16b9568834cb625bc
Robert <wybawiciel@gmail.com>
2022-07-05 22:06:39 +0200

    unfinished

--- app.js
+++ app.js
@@ -20,45 +20,25 @@ window.addEventListener('DOMContentLoaded', init());
 
 function init() {
     isLocalStorage();
-    timerGetRates()
+    // timerGetRates()
     filterMainCurrency()
-    initListeners()
     renderBottomRates()
+    initListeners()
     renderCountryList()
-}
-
-function renderBottomRates() {
-    mainCurrenciesApiData.forEach(el => {
-        let liEl = `
-        <div class="single__rates">
-        <img src="https://countryflagsapi.com/png/${el.country}" alt="${el.country} flag">
-        <div class="single__rates--country">
-            <h3>${el.code}</h3>
-            <p>${el.mid}</p>
-        </div>
-    </div>
-        `
-
-        bottomRatesContainer.innerHTML += liEl;
-    })
-}
-
-function timerGetRates() {
-    setInterval(() => {
-        getData()
-        // DODAĆ RENDER KURSÓW
-    }, 3600000);
 }
 
 function isLocalStorage() {
     const storage = localStorage.getItem(LOCAL_STORAGE_DATA);
     const timeStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_HOUR))
 
+    // is local storage
     if(storage && timeStorage == hour) {
         currenciesApiData = JSON.parse(storage)
-        console.log(currenciesApiData);
+        currFullData = currenciesApiData
+
     } else {
         getData()
+        console.log('dupa');
     }
 }
 
@@ -71,18 +51,19 @@ function getData() {
         currenciesApiData = data[0].rates
             currenciesApiData.push(polandData)
             connectData()
-            console.log(currFullData);
-            localStorage.setItem(LOCAL_STORAGE_DATA, JSON.stringify(currenciesApiData))
+
+            localStorage.setItem(LOCAL_STORAGE_DATA, JSON.stringify(currFullData))
             localStorage.setItem(LOCAL_STORAGE_HOUR, JSON.stringify(hour))
     });
 }
 
 function connectData() {
-    console.log('elo');
-    currenciesApiData.map( el =>{
+    currFullData = currenciesApiData
+    console.log(currFullData);
+
+    currFullData.forEach( el =>{
         currCodeAndCountry.forEach((elSM, index) => {
             if(el.code == elSM.code) {
-                currFullData = currenciesApiData
                 currFullData[index].country = currCodeAndCountry[index].country.toLowerCase()
                 currFullData[index].src = `https://countryflagsapi.com/png/${currFullData[index].country}`
             }
@@ -91,12 +72,12 @@ function connectData() {
 }
 
 function filterMainCurrency() {
+console.log(currenciesApiData);
     currenciesApiData.forEach(el => {
         if(mainCountriesCode.includes(el.code)) {
             mainCurrenciesApiData.push(el)
         }
     })
-    console.log(mainCurrenciesApiData);
 }
 
 function initListeners() {
@@ -114,3 +95,39 @@ function initListeners() {
 function renderCountryList() {
 
 }
+
+
+function renderBottomRates() {
+    console.log(mainCurrenciesApiData);
+    mainCurrenciesApiData.forEach(el => {
+        let liEl = `
+        <div class="single__rates">
+        <img src="https://countryflagsapi.com/png/${el.country}" alt="${el.country} flag">
+        <div class="single__rates--country">
+            <h3>${el.code}</h3>
+            <p>${el.mid}</p>
+        </div>
+    </div>
+        `
+
+        bottomRatesContainer.innerHTML += liEl;
+    })
+}
+
+function timerGetRates() {
+    setInterval(() => {
+        // getData()
+        // DODAĆ RENDER KURSÓW
+    }, 3600000);
+}
+
+
+// <li>
+// <img class="countryList__subtitles_img" src="https://countryflagsapi.com/png/usa" alt="Europe flag">
+
+// <div class="countryList__countryName">
+//     <span class="countryList__countryName--shortName">USD</span>
+
+//     <span class="countryList__countryName--rateName">Amerykański Dolar</span>
+// </div>
+// </li>
